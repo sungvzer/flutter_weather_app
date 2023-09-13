8094063cf6ce1d5fe6060567e13afc80b6e9db08
sungvzer <sungvzer@proton.me>
2023-09-13 22:57:54 +0200

    feat: add riverpod
    Signed-off-by: sungvzer <sungvzer@proton.me>

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_i18n/flutter_i18n.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:logging/logging.dart';
 import 'package:weather_app/pages/home_page/home_page.dart';
 
@@ -18,76 +19,78 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     const color = Color.fromARGB(255, 0x14, 0x25, 0x44);
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      localizationsDelegates: [
-        FlutterI18nDelegate(
-          translationLoader: FileTranslationLoader(
-            useCountryCode: false,
-            // Enable if testing other languages
-            // forcedLocale: const Locale('it'),
+    return ProviderScope(
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        localizationsDelegates: [
+          FlutterI18nDelegate(
+            translationLoader: FileTranslationLoader(
+              useCountryCode: false,
+              // Enable if testing other languages
+              // forcedLocale: const Locale('it'),
+            ),
+            missingTranslationHandler: (key, locale) {
+              Logger.root.warning("Missing Key: $key");
+            },
           ),
-          missingTranslationHandler: (key, locale) {
-            Logger.root.warning("Missing Key: $key");
-          },
-        ),
-      ],
-      // debugShowMaterialGrid: true,
-      title: 'Flutter Demo',
+        ],
+        // debugShowMaterialGrid: true,
+        title: 'Flutter Demo',
 
-      theme: ThemeData(
-        colorSchemeSeed: Colors.blue,
-        fontFamily: 'Poppins',
-        textTheme: const TextTheme(
-          headlineMedium: TextStyle(
-            color: color,
+        theme: ThemeData(
+          colorSchemeSeed: Colors.blue,
+          fontFamily: 'Poppins',
+          textTheme: const TextTheme(
+            headlineMedium: TextStyle(
+              color: color,
+            ),
+            headlineSmall: TextStyle(
+              color: color,
+            ),
+            headlineLarge: TextStyle(
+              color: color,
+            ),
+            displayMedium: TextStyle(
+              color: color,
+            ),
+            displaySmall: TextStyle(
+              color: color,
+            ),
+            displayLarge: TextStyle(
+              color: color,
+            ),
+            labelMedium: TextStyle(
+              color: color,
+            ),
+            labelSmall: TextStyle(
+              color: color,
+            ),
+            labelLarge: TextStyle(
+              color: color,
+            ),
+            titleMedium: TextStyle(
+              color: color,
+            ),
+            titleSmall: TextStyle(
+              color: color,
+            ),
+            titleLarge: TextStyle(
+              color: color,
+            ),
+            bodyMedium: TextStyle(
+              color: color,
+            ),
+            bodySmall: TextStyle(
+              color: color,
+            ),
+            bodyLarge: TextStyle(
+              color: color,
+            ),
           ),
-          headlineSmall: TextStyle(
-            color: color,
-          ),
-          headlineLarge: TextStyle(
-            color: color,
-          ),
-          displayMedium: TextStyle(
-            color: color,
-          ),
-          displaySmall: TextStyle(
-            color: color,
-          ),
-          displayLarge: TextStyle(
-            color: color,
-          ),
-          labelMedium: TextStyle(
-            color: color,
-          ),
-          labelSmall: TextStyle(
-            color: color,
-          ),
-          labelLarge: TextStyle(
-            color: color,
-          ),
-          titleMedium: TextStyle(
-            color: color,
-          ),
-          titleSmall: TextStyle(
-            color: color,
-          ),
-          titleLarge: TextStyle(
-            color: color,
-          ),
-          bodyMedium: TextStyle(
-            color: color,
-          ),
-          bodySmall: TextStyle(
-            color: color,
-          ),
-          bodyLarge: TextStyle(
-            color: color,
-          ),
+          useMaterial3: true,
         ),
-        useMaterial3: true,
+        home: const HomePage(),
       ),
-      home: const HomePage(),
     );
   }
 }
